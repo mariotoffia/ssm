f089d9ffc12cdecad9e6bfda4368cf66f77af3e2
Mario Toffia <mario.toffia@dataductus.se>
2020-04-29 09:14:16 +0200

    documented FullNameField

--- support/fullnamefield.go
+++ support/fullnamefield.go
@@ -7,8 +7,15 @@ import "reflect"
 // pms it could be /prod/test-service/myproperty. It also
 // includes field metadata including the reflect.Value.
 type FullNameField struct {
-	LocalName  string
+	// Local name in dotted navigation format
+	LocalName string
+	// Remove name as required by AWS
+	// (for PMS this is not a ARN)
 	RemoteName string
-	Field      reflect.StructField
-	Value      reflect.Value
+	// The field within the struct that is reffered
+	Field reflect.StructField
+	// The value accessor to the field. Note if this is
+	// a pointer; it may not have a value do check IsValid
+	// before accessing.
+	Value reflect.Value
 }
